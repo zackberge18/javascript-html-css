fb730b8f1ca9ef1df5de0aed46ef568e9a1dbeff
zackberge18 <82992677+zackberge18@users.noreply.github.com>
2021-06-26 18:07:31 -0700

    Add files via upload

---
+++ static/ecma/index.js
@@ -0,0 +1,22 @@
+// const person={
+//   name:"mosh",
+//   walk(){
+//     console.log(this.name);
+//   }
+// }
+//
+// person.walk()
+// const walk=person.walk.bind(person);
+// walk()
+
+
+// 
+// const square=number=>number*number;
+//
+// console.log(square(5));
+//
+// const jobs=[{id:1,isActive:true},
+//             {id:2,isActive:true},
+//             {id:3,isActive:false},]
+// const activeJobs=jobs.filter(function(job){return job.isActive;}
+// const activeJobs=jobs.filter(job=> job.isActive);
---
+++ static/ecma/w3.js
@@ -0,0 +1,89 @@
+// console.log(Math.floor(Math.random()*10));
+//
+//
+// function  getRndInteger(min,max){
+//   return Math.floor(Math.random()*(max-min))+min;
+//
+// }
+//
+// console.log(getRndInteger(2,5));
+//
+//
+// console.log(10>300)
+// console.log(10<300)
+// console.log(10==300300)
+
+//
+// let hour=12
+//
+// if (hour==34){
+//   console.log("helo wor");
+// }else if (hour==59){
+//   console.log("enjoy bitches");
+// }else{
+//   console.log("fuck yo4");
+// }
+
+// let my_Variable=Math.floor(Math.random()*5);
+// switch (my_Variable) {
+//   case 2:
+//   console.log("Helllo")
+//     break;
+//     case 1:
+//     console.log("1")
+//       break;
+//       case 3:
+//       console.log("3")
+//         break;
+//         default:
+//         console.log("it should be 4")
+//       break;
+//
+// }
+
+//
+// console.log(new Date().getDay())
+// let cars=["1","2","3","4","5","6","7"]
+// let text=""
+// for (let i=0; i<cars.length;i++){
+//   text +=cars[i] +"\n"
+//
+// }
+// console.log(text)
+// const person=["jack","bulma"];
+// let text="";
+//
+// for (let x in person){
+//   text+=person[x];
+// }
+// console.log(text)
+
+// let text="visit W3school";
+// let n=text.search(/W3school/i)
+// let result=text.replace("W3school","fuck you")
+// console.log(result);
+// console.log(n);
+
+//
+// try {
+//   consa
+// }
+// catch(err){
+//   console.log("hello world")
+// }
+
+// const person1={
+//   fullName :function(){
+//     return this.firstName
+//   }
+// }
+//
+// const person2={
+//   firstName:"zack"
+// }
+// console.log(person1.fullName.call(person2));
+
+
+let hello =(val)=>"hello world"+val;
+
+console.log(hello("sdrt"));
